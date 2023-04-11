ddbad227cd0c7d89ede41bbe58c1c05224bd0bd8
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2023-04-11 11:58:57 +0200

    fix: update lsp packages

--- lua/henrikig/plugins/lsp/mason.lua
+++ lua/henrikig/plugins/lsp/mason.lua
@@ -23,7 +23,7 @@ mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
 		"tsserver",
-		"sumneko_lua",
+		"lua_ls",
 		"rust_analyzer",
 		"dockerls",
 		"jsonls",
