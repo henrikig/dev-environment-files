23a807966ad2cd02c44eec24a15c89fdcbd93549
Henrik Irgens Gravdal <47174810+henrikig@users.noreply.github.com>
2023-05-01 11:12:14 +0200

    test new catppuccin theme (#1)
    * test new catppuccin theme
    
    * add keybinding for lsp hover

--- lua/henrikig/core/colorscheme.lua
+++ lua/henrikig/core/colorscheme.lua
@@ -1,12 +1,12 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+local status, _ = pcall(vim.cmd, "colorscheme catppuccin-macchiato")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
 end
 
-vim.cmd([[
-let g:nightflyTransparent = 1
-let g:nightflyTerminalColors  = 1
-]])
+-- vim.cmd([[
+-- let g:nightflyTransparent = 1
+-- let g:nightflyTerminalColors  = 1
+-- ]])
