9be5d9f5c73c22eabbe8dd48ba54ff477e658772
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2023-11-11 17:33:04 +0100

    chore: remove comments

--- lua/henrikig/core/colorscheme.lua
+++ lua/henrikig/core/colorscheme.lua
@@ -1,12 +1,5 @@
--- set colorscheme to nightfly with protected call
--- in case it isn't installed
 local status, _ = pcall(vim.cmd, "colorscheme catppuccin-macchiato")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
 end
-
--- vim.cmd([[
--- let g:nightflyTransparent = 1
--- let g:nightflyTerminalColors  = 1
--- ]])
